ba397b0e3e6c4d04e5e95e3956b437733e7403b3
Kacper Marczewski <kacper.marczewski@gmail.com>
2022-10-23 14:53:18 +0200

    cleaning

--- main.py
+++ main.py
@@ -5,14 +5,8 @@ from pymongo import MongoClient
 
 
 def get_database():
- 
-   # Provide the mongodb atlas url to connect python to mongodb using pymongo
    CONNECTION_STRING = "mongodb://testuser:testpass@pi/test"
- 
-   # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
    client = MongoClient(CONNECTION_STRING)
- 
-   # Create the database for our example (we will use the same database throughout the tutorial
    return client['test']
 
 
@@ -24,5 +18,4 @@ if __name__ == '__main__':
     print("collection opened")
     item_details = collection_name.find()
     for item in item_details:
-        # This does not give a very readable output
         print(item)
