7f8be10800aa53461d8702bf2cabb945a786f6bd
Kacper Marczewski <kacper.marczewski@gmail.com>
2024-01-18 01:43:49 +0100

    langchain

--- hw.py
+++ hw.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python3
-
-import sys
-    
-print("Hello World!")
+from langchain_community.llms import Ollama
+ollama = Ollama(base_url='http://localhost:11434', model="phi")
+print(ollama("why is the sky blue"))
