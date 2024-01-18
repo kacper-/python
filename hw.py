8327b804d2a0217fb28faad76747a3127aa6353d
Kacper Marczewski <kacper.marczewski@gmail.com>
2024-01-18 01:54:47 +0100

    warning fix

--- hw.py
+++ hw.py
@@ -1,3 +1,3 @@
 from langchain_community.llms import Ollama
-ollama = Ollama(base_url='http://localhost:11434', model="phi")
-print(ollama("why is the sky blue"))
+ollama = Ollama(model="phi")
+print(ollama.invoke("why is the sky blue"))
