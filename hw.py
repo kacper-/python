fdf72ea27e7fe76a5015d2af5e4302621aed1724
Kacper Marczewski <kacper.marczewski@gmail.com>
2024-01-20 14:09:10 -0500

    prompt added

--- hw.py
+++ hw.py
@@ -1,3 +1,8 @@
 from langchain_community.llms import Ollama
-ollama = Ollama(model="phi")
-print(ollama.invoke("why is the sky blue"))
+
+ollama = Ollama(
+    model="phi",
+    system="you are helpful AI giving short answers"
+    )
+
+print(ollama.invoke("what is capital city of Poland"))
