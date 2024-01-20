b9eb5428f7dbdcd57883f87de5e5e1504a30e43d
Kacper Marczewski <kacper.marczewski@gmail.com>
2024-01-20 23:55:53 +0100

    fix

--- hw.py
+++ hw.py
@@ -1,4 +1,8 @@
 from langchain_community.llms import Ollama
+from langchain_community.document_loaders import UnstructuredPDFLoader
+
+loader = UnstructuredPDFLoader("/Users/kacper/Downloads/Kacper_Marczewski.pdf", mode="elements")
+data = loader.load()
 
 ollama = Ollama(
     model="phi",
